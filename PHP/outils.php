98763d4919a37b7302cd5debc4801a586ce565ac
geranm <matt59210@gmail.com>
2021-04-26 10:17:26 +0200

    MG

---
+++ PHP/outils.php
@@ -0,0 +1,26 @@
+<?php
+
+function chargerClasse($classe){
+    if (file_exists("PHP/controleur/" . $classe . ".class.php")){
+        require "PHP/controleur/" . $classe . ".class.php";
+    }
+    if (file_exists("PHP/modele/" . $classe . ".class.php")){
+        require "PHP/modele/" . $classe . ".class.php";
+    }
+}
+
+function chargerPage($tab){
+    $chemin = $page[0];
+    $nom = $page[1];
+    $titre = $page[2];
+
+    include 'PHP/VIEW/Head.php';
+    include 'PHP/VIEW/Header.php';
+    include 'PHP/VIEW/Nav.php';
+    include $chemin . $nom . '.php'; //Chargement de la page en fonction du chemin et du nom
+    include 'PHP/VIEW/Footer.php';
+}
+
+function crypter($mdp){
+    return md5(md5($mdp).strlen($mdp));
+}    
