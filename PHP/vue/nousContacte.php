1fee95120b14fc2334e610e5cd029e9fac2e5c2a
Jimmy2507 <jimmy.mrchnd@gmail.com>
2021-04-27 15:55:56 +0200

    Nous contacte

---
+++ PHP/vue/nousContacte.php
@@ -0,0 +1,21 @@
+<h1>Nous <span>contacter</span></h1>
+<p>Besoin de conseils ou d'informations, nous sommes là pour vous aider.</p>
+<h2><span>Où nous trouver</span></h2>
+<p>Vous pouvez nous trouvez dans nos locaux</p>
+<h2><span>Service Client :</span></h2>
+<p>
+Vous avez besoin de conseils sur un produit, un service ou une destination. Vous souhaitez obtenir des informations sur un produits.
+Vous pouvez nous joindre :
+<ul>
+    <li>Par téléphone :</li>
+    <p>Nous sommes disponibles du lundi au vendredi de 8h à 19h et le samedi de 8h à 12h. 
+        <br>0613903330
+    </p>
+    <li>Par courrier</li>
+    <p>
+    407 Avenue de la Gironde, 
+    59640 Dunkerque
+    </p>
+    
+</ul>
+</p>
