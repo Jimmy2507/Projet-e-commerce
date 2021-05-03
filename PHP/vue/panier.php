86612475d34d3b3c0bde8f543e043881f0bac653
geranm <matt59210@gmail.com>
2021-05-03 10:02:48 +0200

    MG

--- PHP/vue/panier.php
+++ PHP/vue/panier.php
@@ -11,22 +11,23 @@ if($listePaniers){
     <div class="cadreProduitNom centreHV">'.$produit->getNomProduit().'</div>
     <div class="cadreQuantite centreHV">Quantité : '.$panier->getQteProduit().'</div>
     <div class="cadreProduitPrix centreHV">'.$produit->getPrixProduit().'€</div>
-    <button><a href="?page=actionPaniers&idPa='.$panier->getIdPanier().'&idPr='.$panier->getIdProduit().'&mode=supprimer">Supprimer</a></button></div>
-    <div class="petitEspaceH"></div>';
+    <button><a href="?page=actionPaniers&idPa='.$panier->getIdPanier().'&idPr='.$panier->getIdProduit().'&mode=supprimer">Supprimer</a></button></div>';
     $total += ($produit->getPrixProduit() * $panier->getQteProduit());
-    echo '
-    <div>
-        <div class="centre"></div>
-        <div class="bouton">
-        <div class="centreHV prixTotal">
-            Total : '.$total.' €
-        </div>
-        <button class="Boutton"><a href="?page=accueil">Commander</a></button>
-        </div>
-    </div>';
-    }
+}
 }else{
     echo "Panier vide !";
 }
+
+echo '
+<div class="petitEspaceH"></div>
+<div>
+    <div class="centre"></div>
+    <div class="bouton">
+    <div class="centreHV prixTotal">
+        Total : '.$total.' €
+    </div>
+    <button class="Boutton"><a href="?page=accueil">Commander</a></button>
+    </div>
+</div>';
 
 ?>
